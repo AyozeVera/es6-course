c29337bce9a229bc8ce2998b9c92cbdaea24db7f
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 13:06:10 +0100

    lets convert this functions to arrow functions

--- app/js/index.js
+++ app/js/index.js
@@ -4,19 +4,28 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Spread opperator objects');
+logTitle('Arrow Functions');
 /* coding examples */
-const address = {
-  city: 'LP',
-  country: 'SP',
-  postCode: '35110'
+
+const hello = function() {
+  const es6 = 'ES6';
+  return `Hello ${es6}`;
 };
 
-const name = {
-  firstName: 'Juan',
-  lastName: 'Palomo'
+const powers = [1,2,3,4,5].map(function(number, index) {
+  return Math.pow(number, index);
+});
+
+
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const person = {...address, ...name}
+const milesToKm = function(miles) {
+  return miles * 1.60934;
+};
 
-log(JSON.stringify(person, null, 2))
+log(hello());
+log(powers);
+log(add(100,100));
+log(milesToKm(100));
