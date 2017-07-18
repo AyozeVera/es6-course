572be50359c5b2b5f0268d026fc44265db005077
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 13:41:55 +0100

    javascript on object keys

--- app/js/index.js
+++ app/js/index.js
@@ -7,15 +7,19 @@ import { log, logTitle } from 'logger';
 logTitle('Enchanced Object Properties');
 /* coding examples */
 
-const calculator = name => {
+const pricePropName = "PRICE"
+
+const calculator = (name, price) => {
   return {
     name,
     add(n1, n2) {
       return n1 + n2
-    }
+    },
+    [pricePropName.toLowerCase()] : price
   }
 }
 
-const calc = calculator('casio')
+const calc = calculator('casio', 19.90)
 log(calc.name)
 log(calc.add(10, 20))
+log(calc.price)
