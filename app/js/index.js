9163a31e130a1e8da76f23cc6da69a8920bccf79
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 17:32:23 +0100

    course finished

--- app/js/index.js
+++ app/js/index.js
@@ -7,22 +7,5 @@ import { log, logTitle } from 'logger';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-logTitle('React & ES6');
+logTitle('Course Finished!!');
 /* coding examples */
-
-class MainComponent extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <h1>Hello World</h1>
-    )
-  }
-}
-
-const mountNode = document.getElementById('mountNode');
-render(
-  <MainComponent />
-,mountNode)
