901fb8d306a9ec162ba0705f903fd1d70b028e0a
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 15:48:01 +0100

    creating animal class, with 2 arguments on constructor, and some basic behaviour functions

--- app/js/index.js
+++ app/js/index.js
@@ -6,3 +6,35 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('ES6 Classes');
 /* coding examples */
+
+class Animal {
+  constructor(name, age) {
+    log(`${name} is an animal and was created`)
+    this.name = name;
+    this.age = age;
+  }
+
+  eat() {
+    log(`${this.name} is eating`);
+  }
+
+  sleep() {
+    log(`${this.name} is sleeping`);
+  }
+
+  logAge() {
+    log(`${this.name} is ${this.age} years old`);
+  }
+}
+
+const bobby = new Animal("Bobby", 2);
+bobby.eat();
+bobby.sleep();
+bobby.logAge();
+
+log("---------------")
+
+const marshal = new Animal("Marshall", 3);
+marshal.eat();
+marshal.sleep();
+marshal.logAge();
