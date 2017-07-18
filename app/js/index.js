74f52ad747f1647b66620414c17175f7c42eaccf
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 12:48:01 +0100

    spread operator with object

--- app/js/index.js
+++ app/js/index.js
@@ -4,29 +4,19 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Spread opperator arrays');
+logTitle('Spread opperator objects');
 /* coding examples */
-const arrayOne = ["Maria", "Ana", "Alex"];
-const arrayTwo = ["Said", "Ismael", "Pepe"];
+const address = {
+  city: 'LP',
+  country: 'SP',
+  postCode: '35110'
+};
 
-const concatArray = [...arrayOne, ...arrayTwo];
+const name = {
+  firstName: 'Juan',
+  lastName: 'Palomo'
+};
 
-concatArray.forEach(function(name){
-  log(name)
-})
+const person = {...address, ...name}
 
-const name = 'Marian'
-const nameToArray = [...name]
-nameToArray.forEach(function(letter) {
-  log(letter)
-})
-
-const addNumbers = function(n1, n2, n3) {
-  return n1 + n2 + n3
-}
-
-const numbers = [1,5,9]
-// const addition = addNumbers(numbers[0], numbers[1], numbers[2])
-// Beter way to do this :
-const addition = addNumbers(...numbers)
-log(addition)
+log(JSON.stringify(person, null, 2))
