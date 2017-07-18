3da63f51c4725660c629e5e0ef9e58179dbfa961
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 16:59:36 +0100

    basic introduction to generators

--- app/js/index.js
+++ app/js/index.js
@@ -4,19 +4,21 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Promises & Fetch API');
+logTitle('Generators');
 /* coding examples */
 
-const getRandomUsers = n => {
-  const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`)
-  fetchRandomUsers.then(response => {
-    response.json().then(randomUsers => {
-      randomUsers.results.forEach(user => {
-        const {gender, email} = user;
-        log(`${gender} - ${email}`)
-      })
-    })
-  })
+const getNumbers = function* () {
+  yield 1;
+  yield "Hello";
+  yield true;
+  yield {name: 'Alex'};
+  return "I am done"
 }
 
-getRandomUsers(10);
+const numbersGen = getNumbers();
+
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+log(numbersGen.next().value);
+log(JSON.stringify(numbersGen.next().value));
+log(numbersGen.next().value);
