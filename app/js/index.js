6ff2bf8f317d1de55a6ff1b61a0c9804b7cdb2ed
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 12:39:42 +0100

    spread operator example

--- app/js/index.js
+++ app/js/index.js
@@ -4,11 +4,29 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Template literals');
+logTitle('Spread opperator arrays');
 /* coding examples */
-const name = "Anna"
-const country = "Spain"
-const age = 30
+const arrayOne = ["Maria", "Ana", "Alex"];
+const arrayTwo = ["Said", "Ismael", "Pepe"];
 
-log("Name: " + name + " Country: " + country + " Age: " + age);
-log(`Name: ${name} Country: ${country} Age: ${age}`);
+const concatArray = [...arrayOne, ...arrayTwo];
+
+concatArray.forEach(function(name){
+  log(name)
+})
+
+const name = 'Marian'
+const nameToArray = [...name]
+nameToArray.forEach(function(letter) {
+  log(letter)
+})
+
+const addNumbers = function(n1, n2, n3) {
+  return n1 + n2 + n3
+}
+
+const numbers = [1,5,9]
+// const addition = addNumbers(numbers[0], numbers[1], numbers[2])
+// Beter way to do this :
+const addition = addNumbers(...numbers)
+log(addition)
