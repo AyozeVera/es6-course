1b34d742e3b368c65e88ef3e8c7a16516fc5840c
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 12:18:01 +0100

    default export and import of Animal class

--- app/js/index.js
+++ app/js/index.js
@@ -4,9 +4,8 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-import { add, divide, PI } from './Maths';
+import Animal from './Animal'
 logTitle('Names Exports / Imports');
 /* coding examples */
-log(add(2,2));
-log(divide(2,2));
-log(PI);
+var animal = new Animal();
+log(animal.getClassType());
