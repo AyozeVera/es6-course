927470308c3ef7c764b504220cef50a8cadc8d02
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 13:11:22 +0100

    oneline arroy functions dont need return or brackets, longer yes

--- app/js/index.js
+++ app/js/index.js
@@ -7,23 +7,16 @@ import { log, logTitle } from 'logger';
 logTitle('Arrow Functions');
 /* coding examples */
 
-const hello = function() {
+const hello = () => {
   const es6 = 'ES6';
   return `Hello ${es6}`;
 };
 
-const powers = [1,2,3,4,5].map(function(number, index) {
-  return Math.pow(number, index);
-});
+const powers = [1,2,3,4,5].map((number, index) => Math.pow(number, index));
 
+const add = (n1, n2) => n1 + n2;
 
-const add = function(n1, n2) {
-  return n1 + n2;
-};
-
-const milesToKm = function(miles) {
-  return miles * 1.60934;
-};
+const milesToKm = miles => miles * 1.60934;
 
 log(hello());
 log(powers);
