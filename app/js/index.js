0b92171f1d1c92622eed01094178486d111c51cd
Ayoze Vera <ayo.verad@gmail.com>
2017-07-18 12:12:30 +0100

    Math js created and imported completly into index.js

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,11 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title');
+import * as Math from './Maths';
+logTitle('Names Exports / Imports');
 /* coding examples */
+log(Math.add(2,2));
+log(Math.substract(2,2));
+log(Math.divide(2,2));
+log(Math.multiply(2,2));
+log(Math.PI);
